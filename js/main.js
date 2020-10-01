17235c3264163fa078f734da1e632fdaccdffd52
chithien0909 <chithien0909@gmail.com>
2020-10-01 20:44:54 +0700

    Fix font size, update video ui

--- js/main.js
+++ js/main.js
@@ -129,6 +129,23 @@ $(window).on('load', function() {
 		$("html, body").animate({scrollTop: 0}, 1000);
 	 });
 
+	$(function(){
+		var $refreshButton = $('#refresh');
+		var $results = $('#css_result');
+
+		function refresh(){
+			var css = $('style.cp-pen-styles').text();
+			$results.html(css);
+		}
+
+		refresh();
+		$refreshButton.click(refresh);
+
+		// Select all the contents when clicked
+		$results.click(function(){
+			$(this).select();
+		});
+	});
 
 
 
